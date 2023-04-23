d6290446bf83ff18b93681300fe603a15cca8c7f
Slawik88 <makssmart29@gmail.com>
2023-04-23 08:27:37 -1200

    Fixed file parser.py

--- parsers/parser.py
+++ parsers/parser.py
@@ -1,5 +1,4 @@
 import time
-import cv2
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,67 +17,52 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def process_query(query):
+    # замена пробела на нижнее подчёркивание
+    query = query.replace(" ", "_")
 
-def scrape_icon(query):
-    # создание объекта опций браузера и запуск браузера
-    options = Options()
-    options.headless = True
+    # опции для Firefox
+    options = webdriver.FirefoxOptions()
+    options.set_preference("browser.privatebrowsing.autostart", True)
+    options.set_preference("network.cookie.cookieBehavior", 1)
+    options.set_preference("browser.cookies.lifetimePolicy", 2)
+    options.set_preference("browser.cache.disk.enable", False)
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+
+    # запуск браузера и открытие страницы Google Images
     driver = webdriver.Firefox(options=options)
-    driver.get(f'https://www.google.com/search?q={query}&tbm=isch')
+    driver.get("https://images.google.com/")
 
-    # скроллинг страницы для загрузки большего количества изображений
-    last_height = driver.execute_script('return document.body.scrollHeight')
-    while True:
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        time.sleep(2)
-        new_height = driver.execute_script('return document.body.scrollHeight')
-        if new_height == last_height:
-            break
-        last_height = new_height
+    wait = WebDriverWait(driver, 10)
 
-    # нахождение первого изображения
-    image = None
-    image_url = None
-    for element in driver.find_elements_by_css_selector('img.rg_i'):
-        try:
-            image_url = element.get_attribute('src')
-            if image_url.startswith('http') and not image_url.endswith('svg'):
-                image = requests.get(image_url).content
-                break
-        except:
-            pass
+    try:
+        # нажатие на кнопку "Принять" для согласия с использованием cookies
+        accept_cookie_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]/div')))
+        accept_cookie_button.click()
+    except TimeoutException:
+        pass
 
-    # если изображение не найдено
-    if image is None:
-        driver.quit()
-        return None
+    # поиск поля ввода и ввод запроса
+    search_box = driver.find_element(By.NAME, 'q')
+    search_box.send_keys(f"{query} browser logo, transparent background")
+    search_box.submit()
 
-    # сохранение изображения
+    # ожидание загрузки изображений
+    wait.until(EC.presence_of_element_located((By.XPATH, '//div[@id="islrg"]//img')))
+
+    # получение первого изображения и сохранение его в файл
+    first_image = driver.find_element(By.XPATH, '//div[@id="islrg"]//img')
+    image_base64 = first_image.get_attribute('src').split(',')[1]
+    image_data = base64.b64decode(image_base64)
+
     if not os.path.exists('img\\temp_icon'):
         os.makedirs('img\\temp_icon')
 
     with open(f'img\\temp_icon\\icon_{query}.png', 'wb') as f:
-        f.write(image)
+        f.write(image_data)
 
     driver.quit()
-
-    # обработка изображения и удаление заднего фона
-    image_path = f"img\\temp_icon\\icon_{query}.png"
-    image = cv2.imread(image_path)
-
-    # удаление фона
-    fgbg = cv2.createBackgroundSubtractorMOG2()
-    fgmask = fgbg.apply(image)
-    fg = cv2.bitwise_and(image, image, mask=fgmask)
-
-    if not os.path.exists('img\\temp_icon_not_bg'):
-        os.makedirs('img\\temp_icon_not_bg')
-    cv2.imwrite(f'img\\temp_icon_not_bg\\icon_{query}.png', fg)
-
-    return f'img\\temp_icon_not_bg\\icon_{query}.png'
-
-
-
 
 
 if __name__ == '__main__':
@@ -88,12 +72,9 @@ if __name__ == '__main__':
     print(f"Запросы: {requests_input_list}")
     print(f"Потоки: {requests_input_max_workers}")
     print(f"{Fore.YELLOW}  Начинаем загрузку иконок...  {Style.RESET_ALL}")
-    total_queries = len(requests_input_list)
     with ThreadPoolExecutor(max_workers=requests_input_max_workers) as executor:
         futures = [executor.submit(process_query, query.replace(" ", "_")) for query in requests_input_list]
-        progress_bar = tqdm(total=total_queries)
-        for future, query in zip(futures, requests_input_list):
+        for future, query in tqdm(zip(futures, requests_input_list), total=len(requests_input_list)):
             future.result()
-            progress_bar.update(1)
             tqdm.write(f"{Fore.GREEN }😉 Загрузка завершена для {query}!  {Style.RESET_ALL}")
-    print(f"\n{Fore.GREEN} ✔ Загрузка завершена! {Style.RESET_ALL}") 
+    print(f"\n{Fore.GREEN} ✔ Загрузка завершена! {Style.RESET_ALL}")
