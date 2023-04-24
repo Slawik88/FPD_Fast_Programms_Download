2798134b09316ac9c57671547187096a92eab3df
Slawik88 <makssmart29@gmail.com>
2023-04-24 09:26:07 -1200

    added content for application/messager (icon,info)

--- json_data/insert_data.py
+++ json_data/insert_data.py
@@ -1,23 +1,24 @@
 import json
 import sqlite3
 
-
-with open(r'json_data\browser.json', 'r') as f:
+# открыть json файл и загрузить данные в переменную data
+with open('json_data\\messager.json', 'r') as f:
     data = json.load(f)
 
-
+# установить соединение с базой данных
 conn = sqlite3.connect('data_app.db')
 c = conn.cursor()
 
-for browser in data:
-    app_image_path = data[browser]['app_image_path']
-    app_name = data[browser]['app_name']
-    app_developer = data[browser]['app_developer']
-    app_size = data[browser]['app_size']
-    app_url_website = data[browser]['app_url_website']
-    app_favorites = data[browser]['app_favorites']
+# проитерироваться по данным и добавить каждую запись в таблицу
+for app_name, app_data in data.items():
+    app_image_path = app_data['app_image_path']
+    app_developer = app_data['app_developer']
+    app_size = app_data['app_size']
+    app_url_website = app_data['app_url_website']
+    app_favorites = app_data['app_favorites']
+    c.execute("INSERT INTO message_app (app_image_path, app_name, app_developer, app_size, app_url_website, app_favorites) VALUES (?, ?, ?, ?, ?, ?)",
+              (app_image_path, app_name, app_developer, app_size, app_url_website, app_favorites))
 
-    c.execute("INSERT INTO browser_app (app_image_path, app_name, app_developer, app_size, app_url_website, app_favorites) VALUES (?, ?, ?, ?, ?, ?)", (app_image_path, app_name, app_developer, app_size, app_url_website, app_favorites))
-
+# сохранить изменения и закрыть соединение с базой данных
 conn.commit()
 conn.close()
